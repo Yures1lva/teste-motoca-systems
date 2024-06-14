a8a4272523da6d4b47ff44ed97b2206e34ac9661
Yure <yure.siilva9@gmail.com>
2024-06-14 19:19:48 -0300

    up: estilizando header

--- teste-motocaSystems-frontEnd/src/components/Header/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/Header/index.jsx
@@ -1,13 +1,14 @@
 import './header.module.css'
-
+import home from '../../assets/home.png'
+import perfil from '../../assets/perfil.png'
 
 function Header(){
     return (
         <>
         <div>
             <ul>
-                <li><img src="../assets/Vector.png" alt="" /></li>
-                <li>perfil</li>
+                <li><a href=""><img src={home} alt="pagina incial" /></a></li>
+                <li><a href=""><img src={perfil} alt="página de perfil indisponível" /></a></li>
             </ul>
         </div>
         </>
