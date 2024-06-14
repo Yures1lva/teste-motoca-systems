e64fe9de532ad5915388560046805c28e4efa44a
Yure <yure.siilva9@gmail.com>
2024-06-14 19:06:12 -0300

    add: criando e começando projeto

---
+++ teste-motocaSystems-frontEnd/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import './header.module.css'
+
+
+function Header(){
+    return (
+        <>
+        <div>
+            <ul>
+                <li><img src="../assets/Vector.png" alt="" /></li>
+                <li>perfil</li>
+            </ul>
+        </div>
+        </>
+    )
+} export default Header
---
+++ teste-motocaSystems-frontEnd/src/components/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../../Header"
+
+function Home() {
+    return (
+        <>
+        <Header></Header>
+        <h1>home</h1>
+        </>
+    )
+} export default Home
