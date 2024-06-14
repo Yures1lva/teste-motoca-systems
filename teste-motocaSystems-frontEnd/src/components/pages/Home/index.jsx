0b2d1636004a1b6ba6f303b4abd4087d6039a00e
Yure <yure.siilva9@gmail.com>
2024-06-14 19:55:52 -0300

    estilizando barra incial

--- teste-motocaSystems-frontEnd/src/components/pages/Home/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/pages/Home/index.jsx
@@ -1,10 +1,19 @@
+import styles from'./home.module.css'
 import Header from "../../Header"
+
+
 
 function Home() {
     return (
         <>
         <Header></Header>
-        <h1>home</h1>
+        <div className={styles.barraInicial}>
+            <h3>Tabela de Motos</h3>
+            <ul>
+                <li>Barra de pesquisa</li>
+                <li><button><i>+ </i>NOVO REGISTRO</button></li>
+            </ul>
+        </div>
         </>
     )
 } export default Home
