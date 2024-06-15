8e8fe34b8fd151b986de472b439cb54d0145003c
Yure <yure.siilva9@gmail.com>
2024-06-15 00:28:34 -0300

    up: terminando card da home

--- teste-motocaSystems-frontEnd/src/components/cardHome/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/cardHome/index.jsx
@@ -1,23 +1,33 @@
+/* eslint-disable react/prop-types */
 import trash from '../../assets/trash.png'
 import view from '../../assets/view.png'
 import styles from './card.module.css'
 import Moto from './modelMoto'
 
 
-function CardHome(){
+
+
+// eslint-disable-next-line react/prop-types
+function CardHome({props}){
+ 
+    let meta = [
+        {style: styles.estoque, value:"Em estoque"},
+        {style: styles.semEstoque, value:"sem estoque"},
+        {style: styles.transito, value:"Em transito"}
+    
+    ]
 
     // eslint-disable-next-line no-unused-vars
-    const moto = new Moto({codigo:2, modelo: "POP 110i",cor:"BRANCA",valor:10000.0,status:"Em transito"})
+    const moto = new Moto({codigo: props.codigo, modelo: props.modelo, cor: props.cor, valor: props.valor ,status: props.status})
+    meta [moto.status]
+  
     
     return <>
     
         <article  className={styles.card}>
-
-
-
-                <p>#{moto.codigo}</p>
+                <p>#{("000" + moto.codigo).slice(-4)}</p>
                 <ul>
-                    <li><span>{moto.modelo} <a className={styles.transito} >{moto.status}</a></span></li>
+                    <li><span>{moto.modelo} <a className={meta[moto.status].style} >{meta[moto.status].value}</a></span></li>
                     <li>valor: R$<span>{moto.valor}</span></li>
                     <li>cor: <span> {moto.cor}</span></li>
                 </ul>
--- teste-motocaSystems-frontEnd/src/components/pages/Home/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/pages/Home/index.jsx
@@ -1,24 +1,41 @@
+/* eslint-disable no-unused-vars */
 import Header from "../Header"
 import styles from "./home.module.css"
 import CardHome from "../../../components/cardHome"
-
+import React, { useEffect, useState } from "react"
 
 
 
 
 function Home() {
-    return (
-        <>
-        <Header></Header>
-        <div className={styles.barraInicial}>
-            <h3>Tabela de Motos</h3>
-            <ul>
-                <li>Barra de pesquisa</li>
-                <li><button><i>+ </i>NOVO REGISTRO</button></li>
-            </ul>
-        </div>
-        <CardHome></CardHome>
+    const [cards,setCards] = useState([]);
+   
+    const getCards = () => {
         
-        </>
-    )
+        try {
+             fetch('http://localhost:3000/motos').then((res) => res.json()).then((data) => setCards(data))
+        } catch (error) {
+            console.log("deu erro")
+        }
+    }
+     
+    useEffect(() => {
+        getCards()
+    }, [])
+ 
+     return (
+         <>
+         <Header></Header>
+         <div className={styles.barraInicial}>
+             <h3>Tabela de Motos</h3>
+             <ul>
+                 <li>Barra de pesquisa</li>
+                 <li><button><i>+ </i>NOVO REGISTRO</button></li>
+             </ul>
+         </div>
+         {(cards.map((card) => (<CardHome  key={card.codigo} props={card}/>)))}
+ 
+           
+         </>
+     )
 } export default Home
