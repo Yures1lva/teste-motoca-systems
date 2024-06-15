4e4ad04aa4486d9f9a36b2a875095485e144dda1
Yure <yure.siilva9@gmail.com>
2024-06-14 22:18:25 -0300

    add: criando modelo de moto

--- teste-motocaSystems-frontEnd/src/components/cardHome/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/cardHome/index.jsx
@@ -1,22 +1,25 @@
 import trash from '../../assets/trash.png'
 import view from '../../assets/view.png'
 import styles from './card.module.css'
+import Moto from './modelMoto'
 
 
 function CardHome(){
 
- 
+    // eslint-disable-next-line no-unused-vars
+    const moto = new Moto({codigo:2, modelo: "POP 110i",cor:"BRANCA",valor:10000.0,status:"Em transito"})
+    
     return <>
     
         <article  className={styles.card}>
 
 
 
-                <p>#0001</p>
+                <p>#{moto.codigo}</p>
                 <ul>
-                    <li><span>HONDA POP 110i <a className={styles.transito} >Em transito</a></span></li>
-                    <li>valor: R$<span>15.000</span></li>
-                    <li>cor: <span> BRANCA</span></li>
+                    <li><span>{moto.modelo} <a className={styles.transito} >{moto.status}</a></span></li>
+                    <li>valor: R$<span>{moto.valor}</span></li>
+                    <li>cor: <span> {moto.cor}</span></li>
                 </ul>
                 <div>
                    
---
+++ teste-motocaSystems-frontEnd/src/components/cardHome/modelMoto.jsx
@@ -0,0 +1,11 @@
+ // eslint-disable-next-line no-unused-vars
+ export default class Moto{
+     
+    constructor({codigo, modelo, cor, valor, status}){
+        this.codigo = codigo;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.valor = valor;
+        this.status = status;
+    }
+}
