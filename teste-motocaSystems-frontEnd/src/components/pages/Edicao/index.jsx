2f6726aa4dec29ff186d45c895a159c87906fad1
Yure <yure.siilva9@gmail.com>
2024-06-16 12:55:10 -0300

    add: inserindo título da pagina

--- teste-motocaSystems-frontEnd/src/components/pages/Edicao/index.jsx
+++ teste-motocaSystems-frontEnd/src/components/pages/Edicao/index.jsx
@@ -5,8 +5,8 @@ function Edicao() {
     
     return <>
     <div className={styles.barraInicial}>
-             <h3>Tabela de Motos</h3>
+             <h3>Editar</h3>
         </div>
-    <p>teste</p>
+    <h2>Edite as informações que preferir!</h2>
     </>
 } export default Edicao
